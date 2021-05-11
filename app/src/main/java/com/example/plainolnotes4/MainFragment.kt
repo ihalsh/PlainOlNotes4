8bc83d4ff17108a7b4cde18120a36b868a784202
Ihor Sheremet <igot.sheremet@gmail.com>
2021-05-11 22:32:59 +0300

    Add a navigation controller to the activity

---
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.plainolnotes4
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
